c2eba92e17e3df3106c6cab8f342e5784fd76671
labibks <h.kawsarsheikh@gmail.com>
2025-10-25 19:53:25 +0600

    add .env file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,22 +1,19 @@
-// Import the functions you need from the SDKs you need
+// src/firebase/firebase.config.js
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// Firebase configuration (from .env)
 const firebaseConfig = {
-  apiKey: "AIzaSyBXG8th8e7uRFJliyTO0_PYfSGN8orVB-o",
-  authDomain: "travel-guru-d9a0e.firebaseapp.com",
-  projectId: "travel-guru-d9a0e",
-  storageBucket: "travel-guru-d9a0e.firebasestorage.app",
-  messagingSenderId: "616211015593",
-  appId: "1:616211015593:web:6a6ec93f247eb6008a06b8",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default app;
-// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+export default app;
